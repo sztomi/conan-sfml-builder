0e63e8e9113bb7f512d2dd3ff4748fff68783f52
sztomi <szelei.t@gmail.com>
2017-12-03 14:24:00 +0100

    Use conanfile instead of self

--- sfml_builder.py
+++ sfml_builder.py
@@ -20,13 +20,14 @@ def build_module(conanfile, module_name, source_dir="sources"):
 
 def package_module(conanfile, module_name, source_dir="sources"):
     with tools.chdir(source_dir):
-        self.copy(pattern="LICENSE")
-        self.copy(
+        conanfile.copy(pattern="LICENSE")
+        conanfile.copy(
             pattern="*",
             dst="include/" + module_name,
             src="sources/include/" + module_name)
-        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        conanfile.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+        conanfile.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        conanfile.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+        conanfile.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+        conanfile.copy(
+            pattern="*.dylib", dst="lib", src="lib", keep_path=False)
